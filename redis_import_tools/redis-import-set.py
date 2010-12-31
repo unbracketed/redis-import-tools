7f929fedea18288164754196d38d0736386d00a5
Brian Luft <brian@unbracketed.com>
2010-12-31 10:39:19 -0800

    fixes error causing last insert batch to not get executed

--- redis_import_tools/redis-import-set.py
+++ redis_import_tools/redis-import-set.py
@@ -1,13 +1,10 @@
-# redis-import-set
-import fileinput
+"redis-import-set"
 import sys
-from itertools import count, islice
 
 import redis
 
 
-if __name__ == '__main__':
-	
+def handle():
     r = redis.Redis()
     pipeline_redis = r.pipeline()
     count = 0
@@ -17,9 +14,13 @@ if __name__ == '__main__':
         raise Exception("You must specify the name for the Set")
 
     for line in sys.stdin:
-        pipeline_redis.sadd(keyname, line)
+        pipeline_redis.sadd(keyname, line.rstrip())
         count += 1
         if not count % 10000:
-        	pipeline_redis.execute()
+            pipeline_redis.execute()
+    pipeline_redis.execute()
 
+
+if __name__ == '__main__':
+    handle()
 
