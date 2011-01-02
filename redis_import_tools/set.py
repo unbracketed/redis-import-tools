eb01097014c474619409f1f6e64f05050142ae3b
Brian Luft <brian@unbracketed.com>
2011-01-01 20:49:35 -0800

    handling a few options using argparse

--- redis_import_tools/set.py
+++ redis_import_tools/set.py
@@ -6,20 +6,20 @@ from itertools import groupby
 import redis
 
 
-def handle():
+def handle(**kwargs):
     r = redis.Redis()
     pipeline_redis = r.pipeline()
     count = 0
-    try:
-        keyname = sys.argv[1]
-    except IndexError:
-        raise Exception("You must specify the name for the Set")
-
+#    try:
+#        keyname = sys.argv[1]
+#    except IndexError:
+#        raise Exception("You must specify the name for the Set")
+    
     seen = set()
     for member, _ in groupby(reader(sys.stdin, delimiter='\t'),
     	                    lambda x:x[0]):
         if member not in seen:
-            pipeline_redis.sadd(keyname, member.rstrip())
+            pipeline_redis.sadd(kwargs['key'], member.rstrip())
             count += 1
             seen.add(member)
             if not count % 10000:
@@ -28,5 +28,12 @@ def handle():
 
 
 if __name__ == '__main__':
-    handle()
+    import argparse
+    parser = argparse.ArgumentParser(description='Tools for importing data into Redis.')
+    parser.add_argument('-u', '--unsorted', action="store_true")
+    parser.add_argument('key', required=True)
+    options = parser.parse_args()
+    
+    handle(**options)
+
 
