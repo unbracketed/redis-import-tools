d048b7357dc4f3a6a114383e8adc8e514d41a4d0
Brian Luft <brian@unbracketed.com>
2011-01-03 18:51:07 -0800

    experimenting with gevent

--- redis_import_tools/set.py
+++ redis_import_tools/set.py
@@ -3,6 +3,8 @@ import sys
 from csv import reader
 from itertools import groupby
 
+from gevent import monkey; monkey.patch_socket()
+import gevent
 import redis
 
 
@@ -10,30 +12,21 @@ def handle(**kwargs):
     r = redis.Redis()
     pipeline_redis = r.pipeline()
     count = 0
-#    try:
-#        keyname = sys.argv[1]
-#    except IndexError:
-#        raise Exception("You must specify the name for the Set")
+    key = kwargs['key']
     
-    seen = set()
+    seen = set([None])
     for member, _ in groupby(reader(sys.stdin, delimiter='\t'),
-    	                    lambda x:x[0]):
+    	                    lambda x:x[0] if len(x) else None):
         if member not in seen:
-            pipeline_redis.sadd(kwargs['key'], member.rstrip())
+            pipeline_redis.sadd(key, member.rstrip())
             count += 1
             seen.add(member)
             if not count % 10000:
-                pipeline_redis.execute()
+                gevent.spawn(pipeline_redis.execute).join()
     pipeline_redis.execute()
 
 
 if __name__ == '__main__':
-    import argparse
-    parser = argparse.ArgumentParser(description='Tools for importing data into Redis.')
-    parser.add_argument('-u', '--unsorted', action="store_true")
-    parser.add_argument('key', required=True)
-    options = parser.parse_args()
-    
     handle(**options)
 
 
