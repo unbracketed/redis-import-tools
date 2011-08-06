c16a946796de0a3e6c966fdce7f8723d49b07be5
Brian Luft <brian@unbracketed.com>
2011-08-05 23:08:41 -0700

    list import

--- redis_import_tools/commands.py
+++ redis_import_tools/commands.py
@@ -1,4 +1,4 @@
-"redis-import-set"
+"""wrappers for Redis insert commands"""
 import sys
 from csv import reader
 from itertools import groupby
@@ -7,20 +7,43 @@ import redis
 
 
 def import_set(**kwargs):
+    """
+    """
     r = redis.Redis()
     pipeline_redis = r.pipeline()
     count = 0
     key = kwargs['key']
     batch_size = kwargs['batch_size']
-    
+
     seen = set([None])
     for member, _ in groupby(reader(sys.stdin, delimiter='\t'),
-    	                    lambda x:x[0] if len(x) else None):
+                            lambda x:x[0] if len(x) else None):
         if member not in seen:
             pipeline_redis.sadd(key, member.rstrip())
             count += 1
             seen.add(member)
             if not count % batch_size:
                 pipeline_redis.execute()
+    #send the last batch
+    pipeline_redis.execute()
+
+
+def import_list(**kwargs):
+    """
+    """
+    r = redis.Redis()
+    pipeline_redis = r.pipeline()
+    count = 0
+    key = kwargs['key']
+    batch_size = kwargs['batch_size']
+
+    #seen = set([None])
+    #for member, _ in groupby(reader(sys.stdin, delimiter='\t'),
+                            #lambda x:x[0] if len(x) else None):
+    for line in sys.stdin:
+        pipeline_redis.rpush(key, line.rstrip())
+        count += 1
+        if not count % batch_size:
+            pipeline_redis.execute()
     #send the last batch
     pipeline_redis.execute()
